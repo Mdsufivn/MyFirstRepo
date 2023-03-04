4eb96c026b0725b9a2f44800e3af218b176ff253
Mdsufivn <Mdsufian.ah@outlook.com>
2023-03-04 16:03:21 -0500

    Day end

---
+++ LearnJava/src/ActionItem_Demo/Test_Calculater.java
@@ -0,0 +1,17 @@
+package ActionItem_Demo;
+
+public class Test_Calculater {
+
+	public static void main(String[] args) {
+		// Created an object of Calculator class
+
+		Calculater calc = new Calculater();
+		
+		System.out.println("Addition of A and B is: " + calc.doAdd(25, 12));
+		
+		System.out.println("Subtraction of B and A is: " + calc.doSub(40, 4));
+		
+	}
+
+}
+
