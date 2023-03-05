acd33f9d835b491d73d3017fe3fed2a964515717
Mdsufivn <Mdsufian.ah@outlook.com>
2023-03-05 11:04:17 -0500

    Method note

--- LearnJava/src/Method_Demo/Calculater.java
+++ LearnJava/src/Method_Demo/Calculater.java
@@ -1,12 +1,12 @@
-package ActionItem_Demo;
+package Method_Demo;
 
 public class Calculater {
-
+	// Formula for creating an auditioning query
 	public int doAdd(int a, int b) {
 		int c = a + b;
-		
+		// Asking the system to give you a return statement
 	return c; 
-	}
+	} // Creating a formula for subtraction
 		 public int doSub(int a, int b) {
 			 int c = a - b;
 			 return c;
--- LearnJava/src/Method_Demo/Test_Calculater.java
+++ LearnJava/src/Method_Demo/Test_Calculater.java
@@ -1,4 +1,4 @@
-package ActionItem_Demo;
+package Method_Demo;
 
 public class Test_Calculater {
 
@@ -8,7 +8,6 @@ public class Test_Calculater {
 		Calculater calc = new Calculater();
 		
 		System.out.println("Addition of A and B is: " + calc.doAdd(25, 12));
-		
 		System.out.println("Subtraction of B and A is: " + calc.doSub(40, 4));
 		
 	}
