c4454e7ffaa929ecb68b5c3a063890b8f73d9884
Mdsufivn <Mdsufian.ah@outlook.com>
2023-03-05 12:35:40 -0500

    Check point

--- LearnJava/src/Array/Pattern.java
+++ LearnJava/src/Array/Pattern.java
@@ -3,8 +3,21 @@ package Array;
 public class Pattern {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+
+		int i, j, k, row = 7;
+		 
+		for (i=1; i<= row; i++) {
+			for (j=i+1; j<=row; j++) {
+				System.out.print(" ");
+			}
+			for (k=1; k<i*2; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("Hurrah!");
 	}
 
 }
