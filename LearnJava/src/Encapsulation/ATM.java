f906278a319746c86a89f36061c6a19414c7c529
Mdsufivn <Mdsufian.ah@outlook.com>
2023-03-11 11:40:05 -0500

    check

--- LearnJava/src/Encapsulation/ATM.java
+++ LearnJava/src/Encapsulation/ATM.java
@@ -6,10 +6,16 @@ public class ATM {
 		
 		Bank obj = new Bank();
 		// obj.pinNumber = 1234;
-		obj.updatePin(12345678, 1234, 1222);
-		obj.drawMoney(12345678, 1234, 1000);
+//		obj.updatePin(12345678, 1234, 1222);
+//		obj.drawMoney(12345678, 1234, 1000);
 
-
+		//unchecked exception
+//		int[] number = {5, 9, 2,78};
+//		System.out.println(number[4]);
+		
+		int a = 5, results;
+		results = 5/1; 
+		System.out.println(results);
 	}
 
 }
---
+++ LearnJava/src/Inheritance/Animal.java
@@ -0,0 +1,7 @@
+package Inheritance;
+
+public class Animal {
+
+	// Inheritance is the process/concept of inheriting the properties and behaviors of one class
+	// from another class
+}
